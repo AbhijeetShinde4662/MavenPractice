4c0ec2d5396cbafde7f54a1f259dcc718bff79d9
Shinde <Shinde@LAPTOP-T5I20ICV>
2022-09-23 12:57:05 +0530

    Project Updated

--- src/test/java/PracticePackage/Practice1Test.java
+++ src/test/java/PracticePackage/Practice1Test.java
@@ -7,6 +7,12 @@ public class Practice1Test
 	@Test
 	public void sample()
 	{
+		String browser = System.getProperty("br");
+		String url = System.getProperty("ur");
+		
+		System.out.println(browser);
+		System.out.println(url);
 		System.out.println("Hi I am Indian");
 	}
 }
+ 
