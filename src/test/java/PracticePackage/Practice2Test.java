fb071cae3cf774c8d40b53c975aea97a7b5f61ba
Shinde <Shinde@LAPTOP-T5I20ICV>
2022-09-23 11:15:18 +0530

    Maven Project updated

---
+++ src/test/java/PracticePackage/Practice2Test.java
@@ -0,0 +1,12 @@
+package PracticePackage;
+
+import org.testng.annotations.Test;
+
+public class Practice2Test 
+{
+	@Test
+	public void sample()
+	{
+		System.out.println("Hi I am Maratha");
+	}
+}
